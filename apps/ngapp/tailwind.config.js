b7d2e885941c44110c0c0672be87caa4ea704aaf
Manuel Bertoluzza <bertoluzza@gmail.com>
2025-04-05 16:50:15 +0200

    style: adds basic styling with tailwind and scss folder structure
    Sets up Tailwind CSS for basic styling, including:
    - Adds base, tokens, utils, and vendors SCSS folders
    - Configures basic colors and spacing
    - Restyles default html elements
    
    Updates the Angular project configuration to include Tailwind.
    Also increases the bundle size warning limit.

--- apps/ngapp/tailwind.config.js
+++ apps/ngapp/tailwind.config.js
@@ -4,11 +4,30 @@ const { join } = require('path');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    join(__dirname, 'src/**/!(*.stories|*.spec).{ts,html}'),
+    join(__dirname, './src/**/!(*.stories|*.spec).{ts,html}'),
     ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
-    extend: {},
+    extend: {
+      // fontFamily: {
+      //   sans: ['Roboto', 'Helvetica Neue', 'sans-serif'],
+      // },
+    },
+    // colors: {
+      // blue: {
+      //   // should be same as in styles.scss
+      //   50: '#eff6ff',
+      //   100: '#dbeafe',
+      //   200: '#bfdbfe',
+      //   300: '#93c5fd',
+      //   400: '#60a5fa',
+      //   500: '#3b82f6',
+      //   600: '#4c73db',
+      //   700: '#2852c8',
+      //   800: '#2144a6',
+      //   900: '#1d3c91',
+      // },
+    // },
   },
   plugins: [],
 };
